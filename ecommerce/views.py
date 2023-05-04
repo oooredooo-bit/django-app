279606b96cd8f773857ce4d05eed6b576b5689e3
Noel Antonio <noel.antoniooo18@gmail.com>
2023-05-04 18:25:14 +0800

    Code refactor on views.py using appconfig

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,16 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpRequest
-from django.conf import settings
+from django.apps import apps
+
+# Getting application config
+app_config = apps.get_app_config(__name__.split('.')[0])
+app_name = app_config.name
 
 # Create your views here.
 def index(request: HttpRequest):
-    return render(request, "ecommerce/index.html")
+    return render(request, f"{app_name}/index.html")
 
 def about(request: HttpRequest):
-    return render(request, "ecommerce/about.html")
+    return render(request, f"{app_name}/about.html")
 
 def gallery(request: HttpRequest):
-    return render(request, "ecommerce/gallery.html")
+    return render(request, f"{app_name}/gallery.html")
 
 def contact(request: HttpRequest):
-    return render(request, "ecommerce/contact.html")
+    return render(request, f"{app_name}/contact.html")
