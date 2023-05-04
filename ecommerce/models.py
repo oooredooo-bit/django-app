cfe5386e8a582ae3a2211d76acd050979c250672
Noel Antonio <noel.antoniooo18@gmail.com>
2023-05-04 20:17:58 +0800

    Create model and migrations. Add to Django Admin

--- ecommerce/models.py
+++ ecommerce/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Inquiries(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    phone_no = models.CharField(max_length=12)
+    message = models.TextField()
